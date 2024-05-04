38b81d8ce32691a8aaeb09fb537a0e68fe5bf0d3
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-05-04 11:51:38 +0300

    added a new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("This is my first repository");
+        System.out.println("This is second commit");
     }
 }
